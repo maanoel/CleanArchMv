405dad9544d0f6fad039d29507eb8d00bea16ac3
unknown <manoelvitorbrito@gmail.com>
2024-03-30 14:56:02 -0300

    feat: Create service to use Identity autenticating with Entity framework

---
+++ CleanArcMvc.Domain/Account/IAutenticate.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchMvc.Domain.Account
+{
+    public interface IAutenticate
+    {
+        Task<bool> Authenticate(string email, string password); 
+
+        Task<bool> RegisterUser(string email, string password);
+
+        Task Logout();
+    } 
+}
---
+++ CleanArcMvc.Infra.Data/Identity/AutenticateService.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class AutenticateService : IAutenticate
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AutenticateService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicaitonUser = new ApplicationUser
+            {
+                Email = email,
+                UserName = email,
+            };
+
+            var result = await _userManager.CreateAsync(applicaitonUser);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(applicaitonUser, isPersistent: false);
+            }
+
+            return result.Succeeded;
+        }
+    }
+}
