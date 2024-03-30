ff7e6f7a791ec6e63f19fe7ab59556f1cbb6b7b4
unknown <manoelvitorbrito@gmail.com>
2024-03-30 15:12:06 -0300

    study: Seeding SQL Server with initial data.

---
+++ CleanArcMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,76 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    internal class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<ApplicationUser> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedRoles()
+        {
+            if (!_roleManager.RoleExistsAsync("User").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "User";
+                role.NormalizedName = "USER";
+
+                IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+            }
+
+            if (!_roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Admins";
+                role.NormalizedName = "ADMIN";
+                IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+            }
+        }
+
+        public void SeedUsers()
+        {
+            if (_userManager.FindByEmailAsync("usuario@localhost").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "usuario@localhost";
+                user.Email = "usuario@localhost";
+                user.NormalizedUserName = "USUARIO@LOCALHOST";
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = _userManager.CreateAsync(user, "Numsey#2021").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+
+            if (_userManager.FindByEmailAsync("admin@localhost").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "admin@localhost";
+                user.Email = "admin@localhost";
+                user.NormalizedUserName = "admin@LOCALHOST";
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = _userManager.CreateAsync(user, "Numsey#2021").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+    }
+}
